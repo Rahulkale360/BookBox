828925ff91d550a832a604ee1c53d5f60f8ce285
unknown <rahulkale360@gmail.com>
2020-11-01 15:47:17 +0530

    changed package.json

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/book-box'));
+app.use(express.static(__dirname + '/dist/BookBox'));
 
 app.get('',function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/book-box/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/BookBox/index.html'))
 })
 
  app.listen(process.env.PORT , 8080);
