86523c34adb8216a3ab740214bc737c957971a19
unknown <rahulkale360@gmail.com>
2020-11-01 15:32:54 +0530

    updated package.json

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/bookbox'));
+app.use(express.static(__dirname + '/dist/book-box'));
 
 app.get('',function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/bookbox/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/book-box/index.html'))
 })
 
  app.listen(process.env.PORT , 8080);
